01f268c6ccc8db689638a5ef1652203d62c6ad4f
Mikkel Bendix Munk <Mikkelbendixmunk@gmail.com>
2015-11-05 11:29:38 +0100

    Wtf

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,41 @@
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
 
+/**
+ * Main styles
+ */
+gulp.task('less', function() {
+    gulp.src('assets/less/**/*.less')
+        .pipe(plugins.changed('www/assets/css'))
+        .pipe(plugins.less())
+        .pipe(plugins.minifyCss())
+        .pipe(gulp.dest('www/assets/css'));
+});
+
+/**
+ * Main scripts
+ */
+gulp.task('scripts', function() {
+    gulp.src('assets/js/**/*.js')
+        .pipe(plugins.changed('www/assets/js'))
+        .pipe(plugins.concat('main.js'))
+        .pipe(gulp.dest('www/assets/js'));
+});
+
+
+
+/**
+ * The default gulp task
+ */
 gulp.task('default', function() {
-    // default task code
+    gulp.start('less', 'scripts');
+});
+
+/**
+ * Keep an eye on these folders when running 'gulp watch' and start the defined scripts if changes appear.
+ * (gulp-changed plugin will make sure only files that are edited will be re-compiled by gulp)
+ */
+gulp.task('watch', function() {
+    gulp.watch('assets/less/**/*.less', ['less']);
+    gulp.watch('assets/js/**/*.js', ['scripts']);
 });
