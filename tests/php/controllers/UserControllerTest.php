2dff27dbc3af463006bb360387c6a63c29814fea
Mikkel Bendix Munk <Mikkelbendixmunk@gmail.com>
2015-11-10 16:15:42 +0100

    Some changes to UserControllerTest

--- tests/php/controllers/UserControllerTest.php
+++ tests/php/controllers/UserControllerTest.php
@@ -2,15 +2,30 @@
 
 namespace tests\php\controllers;
 
-use App\Http\Controllers\User;
+use App\Http\Controllers\UserController;
 
 class UserControllerTest extends \PHPUnit_Framework_TestCase {
 
     public function testCreateUser() {
-        $
+        $name = 'navn';
+        $username = 'mbk';
+        $password = 'secret';
+        $role = 'boss'; 
 
-        $userMock->expects($this->once())
-                ->method('createUser')
-                ->with($this->equalTo('newUser'));
+        $userControllerMock = $this->getMockBuilder('UserController')
+            ->setMethods(array('createUser'))
+            ->getMock();
+
+        $userControllerMock->expects($this->once())
+            ->method('createUser')
+            ->with($name, $username, $password, $role)
+            ->willReturn(true);
+
+        $this->assertTrue($userControllerMock->createUser($name, $username, $password, $role));
+
+        // gets some data
+        // creates user object
+        // calls userRepo->save(user object)
+        // returns true
     }
 }
