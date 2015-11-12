f73925b0dbb2a080afb8769ce96900e3694d798a
Mikkel Bendix Munk <Mikkelbendixmunk@gmail.com>
2015-11-12 16:13:47 +0100

    That's how I role

---
+++ resources/views/frontpage.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <h1>Hej</h1>
+    </head>
+<body ng-app="statistic">
+    {% block content %}
+    {% endblock %}
+</body>
+<footer>
+    <script src="node_modules/angular/angular"></script>
+    <p>Copyright</p>
+</footer>
+</html>
