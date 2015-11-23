256ba882a696506fc33b98c751ed4e4f850959db
Mikkel Bendix Munk <Mikkelbendixmunk@gmail.com>
2015-11-23 16:17:08 +0100

    Changed master template css file

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -12,9 +12,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular.min.js"></script>
     <script src="http://angular-ui.github.io/ui-router/release/angular-ui-router.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.2/angular-resource.js"></script>
-
-    <script src="main.js"></script>
-    <link rel="stylesheet" type="text/css" href="home.css"></script>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
@@ -23,5 +20,11 @@
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+    <!-- Scripts -->
+    <script src="main.js"></script>
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="style.css"></script>
 </footer>
 </html>
