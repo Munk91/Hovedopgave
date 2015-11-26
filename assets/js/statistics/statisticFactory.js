440b46175941023b14e948c8fdb05211317d2c4d
Mikkel Bendix Munk <Mikkelbendixmunk@gmail.com>
2015-11-26 11:26:53 +0100

    Edit api call to be correct

--- assets/js/statistics/statisticFactory.js
+++ assets/js/statistics/statisticFactory.js
@@ -1,4 +1,4 @@
 angular.module('statistic.service', ['ngResource'])
     .factory('showStatistics', function($resource) {
-        return $resource('/api/showStatistics/:statsIndexId/:statsTypeId', { statsIndexId : '@statsIndexId', statsTypeId : '@statsTypeId' });
+        return $resource('/api/elasticGet/:statsIndexId/:statsTypeId', { statsIndexId : '@statsIndexId', statsTypeId : '@statsTypeId' });
     });
