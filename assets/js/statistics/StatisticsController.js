f575d992fcd7a8f8d2b8271844ad086b1ef9306a
Christoffer <cfk@ordbogen.com>
2015-12-02 14:19:35 +0100

    es-frontend-missing-lodash

--- assets/js/statistics/StatisticsController.js
+++ assets/js/statistics/StatisticsController.js
@@ -5,8 +5,8 @@ angular.module('statistics', ['statistic.service'])
         // TODO: Dynamically load this data from the database on load
         ctrl.statistics = [
             {
-                'index' : 'MCstats',
-                'type' : ['Brugere', 'Test']
+                'index' : 'mcstats',
+                'type' : ['brugere', 'Test']
             },
             {
                 'index' : 'Andet',
@@ -21,23 +21,34 @@ angular.module('statistics', ['statistic.service'])
             $state.go('.', {'statsIndexId' : null, 'statsTypeId' : null});
             setActiveDropdownValue();
         };
+        filterData = function(list) {
+            flist = _.map(list);
+            ctrl.filteredList = _.filter(flist[0], function(k, v) {
+                return k
+            });
+            console.log(ctrl.filteredList)
+        }
 
         ctrl.showStatistics = function(statisticIndex, statisticType) {
             statsIndexId = statisticIndex;
             statsTypeId = statisticType;
 
-            showStatistics.query({
+            showStatistics.get({
                 statsIndex : statsIndexId,
                 statsType : statsTypeId
                 })
                 .$promise.then(function(data) {
-                    ctrl.fetchedData = data;
+                    console.log(data);
+                    ctrl.fetchedData = data.hits.hits[0];
+                    filterData(ctrl.fetchedData._source.type);
+                    $state.go('.data');
                 });
         };
 
         ctrl.getStatsTypes = function(selectedStatistic) {
 
             // If index dropdown value changes, reset type dropdown and URL
+            $stateParams.statsIndexId = selectedStatistic.index;
             ctrl.activeStatisticTypeList = [];
             $state.go('.', {'statsIndexId' : selectedStatistic.index, 'statsTypeId' : null});
 
