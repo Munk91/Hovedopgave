a6a08e81e4f64b4cc3deaa8027ee9898adb40c63
Mikkel Bendix Munk <Mikkelbendixmunk@gmail.com>
2015-12-14 10:51:27 +0100

    Better error handling

--- assets/js/pull/pullController.js
+++ assets/js/pull/pullController.js
@@ -13,28 +13,39 @@ angular.module('statistic.pull', ['statistic.pull.service'])
                         ctrl.error = '';
                     }, 3000);
                 } else {
-                    ctrl.success = api + ' blev gemt';
-                    $timeout(function() {
-                        ctrl.success = '';
-                    }, 3000);
+                    var apiES = {
+                        'index': index,
+                        'type': type,
+                        'body': { type: apiData }
+                    };
+
+                    // Index the api data in ElasticSearch
+                    elasticIndex.save(apiES).$promise.then(function() {
+                        var apiRemote = {
+                            'index' : index,
+                            'type' : type,
+                            'url' : api
+                        };
+
+                        // Save the api data in sql db
+                        saveRemoteApi.save(apiRemote).$promise.then(function() {
+                            ctrl.success = api + ' blev gemt';
+                            $timeout(function() {
+                                ctrl.success = '';
+                            }, 3000);
+                        }, function(error) {
+                            ctrl.error = 'Kunne ikke gemme api url i databasen. Prøv igen, eller kontakt admin.';
+                            $timeout(function() {
+                                ctrl.error = '';
+                            }, 3000);
+                        });
+                    }, function(error) {
+                        ctrl.error = 'Kunne ikke indexere data. Tjek om de indtastede data er korrekte og prøv igen.';
+                        $timeout(function() {
+                            ctrl.error = '';
+                        }, 3000);
+                    });
                 }
-
-                var apiES = {
-                    'index': index,
-                    'type': type,
-                    'body': { type: apiData }
-                };
-
-                var apiRemote = {
-                    'index' : index,
-                    'type' : type,
-                    'url' : api
-                }; 
-
-                // Index the api data in ElasticSearch
-                elasticIndex.save(apiES);
-                // Save the api data in sql db
-                //saveRemoteApi.save(apiRemote);
             }, function(error) {
                     ctrl.error = 'Der gik noget galt. Tjek om de indtastede data er korrekt og prøv igen.';
                     $timeout(function() {
